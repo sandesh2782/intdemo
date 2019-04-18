a4bd09318e3b456f4522b1cf28691c48b0e45cf7
Sandesh Magdum <sandesh.s.magdum@gmail.com>
2019-04-18 12:18:18 +0530

    Added auth guard

---
+++ client/src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { AuthenticationService, TokenPayload } from '../authentication.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  credentials: TokenPayload = {
+    email: '',
+    password: ''
+  };
+
+  constructor(
+    private auth: AuthenticationService,
+    private router: Router
+  ) { }
+
+  login(email: string, password: string) {
+    this.auth.login(this.credentials).subscribe(() => {
+      this.router.navigateByUrl('/profile');
+    }, (err) => {
+      console.error(err);
+    });
+  }
+
+}
